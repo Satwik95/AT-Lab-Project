1570a177a28786659f31d815c648b20d3e6ffa3e
satwik mishra <satwik mishra>
2017-03-23 22:24:39 +0530

    adding circle display

---
+++ app/src/main/java/com/example/tstp/splashscreen/finance.java
@@ -0,0 +1,26 @@
+package com.example.tstp.splashscreen;
+
+/**
+ * Created by satwik on 23-03-2017.
+ */
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class finance {
+
+
+}
